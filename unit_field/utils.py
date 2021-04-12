7358257b9a110e04a20c222905d6e47e862a9c04
Christoph Haunschmidt <c.haunschmidt@gmail.com>
2021-04-12 18:05:15 +0200

    Use Django's native django.utils.formats.sanitize_separators if possible

--- unit_field/utils.py
+++ unit_field/utils.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-import six
 import unicodedata
 
+import six
+from django import VERSION as DJANGO_VERSION
 from django.conf import settings
 from django.utils.formats import get_format
 
@@ -10,12 +11,22 @@ Use Django's django.utils.formats.sanitize_formats of Version 1.8
 to guarantee the same behaviour in older Django versions (<= 1.7)
 """
 
-def sanitize_separators(value):
+
+def sanitize_separators(value, force_custom_func=False):
     """
     Sanitizes a value according to the current decimal and
     thousand separator setting. Used with form field input.
+
+    force_custom_func: do not use Django's native sanitize_separators, even if it is available in a
+        proper version
     """
+
     if settings.USE_L10N and isinstance(value, six.string_types):
+        if (DJANGO_VERSION[0] > 1 or (DJANGO_VERSION[0] == 1 and DJANGO_VERSION[1] >= 8)) and not force_custom_func:
+            # Better to use the current native version if possible...
+            from django.utils.formats import sanitize_separators as django_sanitize_separators
+            return django_sanitize_separators(value)
+
         parts = []
         decimal_separator = get_format('DECIMAL_SEPARATOR')
         if decimal_separator in value:
@@ -24,13 +35,13 @@ def sanitize_separators(value):
         if settings.USE_THOUSAND_SEPARATOR:
             thousand_sep = get_format('THOUSAND_SEPARATOR')
             if thousand_sep == '.' and value.count('.') == 1 and \
-                len(value.split('.')[-1]) != 3:
+                    len(value.split('.')[-1]) != 3:
                 # Special case where we suspect a dot meant decimal separator
                 # (see #22171)
                 pass
             else:
                 for replacement in set([
-                    thousand_sep, unicodedata.normalize('NFKD', thousand_sep)]):
+                        thousand_sep, unicodedata.normalize('NFKD', thousand_sep)]):
                     value = value.replace(replacement, '')
         parts.append(value)
         value = '.'.join(reversed(parts))
